af69de1b9ebefdcae852537e68c9b5efe70f2eb4
You Young June <youngjune@planorban.com>
2024-07-15 23:47:15 -0400

    set up pictures

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { UserButton } from "@clerk/nextjs";
-import { Box, Grid, GridCol } from "@mantine/core";
+import { Box, Center, Grid, GridCol } from "@mantine/core";
 
 export default function Home() {
   return (
@@ -13,24 +13,21 @@ export default function Home() {
                 display: "flex",
                 justifyContent: "center",
                 alignItems: "center",
-                height: "95vh",
-              }}
-            >
-              Test
-            </Box>
-          </GridCol>
-          <GridCol span={4}>
-            <Box
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                height: "95vh",
+                height: "93vh",
               }}
             >
               <div>
-                <p>1 2,</p>
-                <p>I love you so much.</p>
+                <Center>
+                  <h3 style={{ fontStyle: "italic" }}>from here...</h3>
+                </Center>
+                <p>
+                  <Center>
+                    <img
+                      src="https://utfs.io/f/7fcd8304-517e-4238-82ed-e1f8abe7241d-1xbyc9.JPG"
+                      style={{ height: "50%", width: "55%" }}
+                    />
+                  </Center>
+                </p>
               </div>
             </Box>
           </GridCol>
@@ -40,15 +37,49 @@ export default function Home() {
                 display: "flex",
                 justifyContent: "center",
                 alignItems: "center",
-                height: "95vh",
+                height: "93vh",
               }}
             >
-              Test
+              <div>
+                <h3>1 2,</h3>
+                <h3>I love you so much.</h3>
+              </div>
+            </Box>
+          </GridCol>
+          <GridCol span={4}>
+            <Box
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "93vh",
+              }}
+            >
+              <div>
+                <Center>
+                  <h3 style={{ fontStyle: "italic" }}>...to here</h3>
+                </Center>
+                <p>
+                  <Center>
+                    <img
+                      src="https://utfs.io/f/2feb26a1-b7a1-4688-9143-4dce1eeaefdb-ip9y3n.jpeg"
+                      style={{ height: "50%", width: "50%" }}
+                    />
+                  </Center>
+                </p>
+              </div>
             </Box>
           </GridCol>
         </Grid>
       </div>
-      <div style={{ justifyContent: "right" }}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "right",
+          paddingRight: "20px",
+          paddingBottom: "20px",
+        }}
+      >
         <UserButton />
       </div>
     </main>
